0e0f2448d4397c9fcaa8dc3cf96df3e2dcd62bd9
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-10-01 09:40:56 +0700

    Add simple ui button

---
+++ Scripts/UI/UIWatchAdmobRewardedVideoAdsButton.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Button))]
+public class UIWatchAdmobRewardedVideoAdsButton : MonoBehaviour
+{
+    public enum WatchAdsButtonType
+    {
+        RewardCurrency,
+        RewardProduct,
+        Custom
+    }
+    public WatchAdsButtonType buttonType;
+    public AdmobRewardedVideoAdSetting setting;
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+
+        switch (buttonType)
+        {
+            case WatchAdsButtonType.RewardCurrency:
+                setting.onRewarded = (reward) =>
+                {
+                    MonetizationManager.Save.AddCurrency(reward.Type, (int)reward.Amount);
+                };
+                break;
+            case WatchAdsButtonType.RewardProduct:
+                setting.onRewarded = (reward) =>
+                {
+                    for (var i = 0; i < (int)reward.Amount; ++i)
+                    {
+                        MonetizationManager.Save.AddPurchasedItem(reward.Type);
+                    }
+                };
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void OnClick()
+    {
+        setting.ShowAd();
+    }
+}
